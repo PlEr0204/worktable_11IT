1f96dddb2b6eab262fa8c1f863f9dc4e07090909
PlEr0204 <platon.erofeev@gmail.com>
2024-11-15 14:27:24 +0300

    15.11.2024

---
+++ Graf/DZ.py
@@ -0,0 +1,23 @@
+N=int(input())
+W=[list(map(int, input().split())) for i in range(N)]
+col=[i for i in range(N)]
+ostov=[]
+Sum_ostov=0
+for k in range(N-1):
+    minDist=100000000000000000000000000000000000000000000000000000000000000
+    for i in range(N-1):
+        for j in range(i+1,N):
+            if col[i]!=col[j]  and W[i][j]!=0 and W[i][j]<minDist:
+                iMin=i
+                jMin=j
+                minDist=W[i][j]
+    ostov.append((iMin,jMin))
+    c=col[jMin]
+    for i in range(N):
+        if col[i]==c:
+            col[i]=col[iMin]
+ostov=sorted(ostov)
+for edge in ostov:
+    Sum_ostov+=W[edge[0]][edge[1]]
+    print(edge[0]+1,edge[1]+1)
+
